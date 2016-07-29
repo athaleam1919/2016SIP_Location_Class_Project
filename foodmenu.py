4d66040d3af50053bb3bac7f98afe9ba0613380f
Athalea McMaster <lilly.mc2724@gmail.com>
2016-07-29 10:30:41 -0400

    This is my foodmenu file

---
+++ foodmenu.py
@@ -0,0 +1,34 @@
+from turtle import *
+def draw_triangle():
+    for i in range(1):
+		pendown()
+		pencolor('black')
+		forward(100)
+		left(120)
+		forward(100)
+		left(120)
+		forward(100)
+		penup()
+
+def draw_triangle():
+    for i in range(1):
+		pendown()
+		pencolor('black')
+		forward(100)
+		left(120)
+		forward(100)
+		left(120)
+		forward(100)
+		penup()
+
+def draw_triangle():
+    for i in range(1):
+		pendown()
+		pencolor('black')
+		forward(100)
+		left(120)
+		forward(100)
+		left(120)
+		forward(100)
+		penup()
+draw_triangle() 
